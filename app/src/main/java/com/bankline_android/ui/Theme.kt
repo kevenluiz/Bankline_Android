adcecc98040fb64877500d55b7c1352d847c9f13
Keven Luiz <kevenluizsojesus@gmail.com>
2024-12-05 05:29:22 -0300

    update xml welcome activity, AndroidManifest.xml and Theme.kt
    Signed-off-by: Keven Luiz <kevenluizsojesus@gmail.com>

--- app/src/main/java/com/bankline_android/ui/Theme.kt
+++ app/src/main/java/com/bankline_android/ui/Theme.kt
@@ -1,6 +1,5 @@
 package com.bankline_android.ui
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
